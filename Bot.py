c53aa1e8c30e20af18b6bfd212510ec34c659f51
Vinlock <dak@pvpallday.com>
2016-03-09 06:55:26 -0800

    Fixed betting error

--- Bot.py
+++ Bot.py
@@ -56,7 +56,7 @@ class Bot(object):
                     for role in member.roles:
                         self.roles[member.server.id][role.name]['members'].append(member)
 
-            # self.thread(self.updateMembers)
+            self.thread(self.updateMembers)
 
             print("Finished creating dictionaries for Roles, Channels, and Possible Match Servers.")
 
