c9dd4e0d50edbce9743099737f05fca47d3dc731
Vinlock <dak@pvpallday.com>
2016-04-16 19:30:22 -0700

    Fixed !pm and !masspm

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
 # import ControlBot
-import Bot
+import Bot, threading
+
+def thread(self, function):
+    t1 = threading.Thread(target=function)
+    t1.daemon = True
+    t1.start()
 
 print("Starting Bot...")
 bot = Bot.Bot()
