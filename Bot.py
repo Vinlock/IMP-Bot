6a33f7210d89295e27b49e2e2ce591314325aee6
Vinlock <dak@pvpallday.com>
2016-04-17 06:07:11 -0700

    Updated !guess logic to 10%

--- Bot.py
+++ Bot.py
@@ -192,17 +192,17 @@ class Bot(object):
                         self.points.minusPoints(bet, message.server.id, message.author.id)
                         await sender(who.mention + " - You have been challenged by " + message.author.mention + " in a roll off out of **" + str(max) + "** for **" + str(bet) + "** points.\nReply \"yes\" to accept. You have 30 seconds.")
                         answer = await waitfor(30, who)
-                        if "yes" in answer.content:
+                        if "yes" in answer.content.lower():
                             self.points.minusPoints(bet, message.server.id, who.id)
                             await sender(who.mention + " has accepted " + message.author.mention + "'s challenge.")
                             await sender(message.author.mention + " - you may roll now with \"!roll\". You have 30 seconds.")
                             firstroll = await wait(30)
-                            if "!roll" in firstroll.content:
+                            if "!roll" in firstroll.content.lower():
                                 roll1 = randint(1, max)
                                 await reply("You have rolled **" + str(roll1) + "**. Good Luck!")
                                 await sender(who.mention + " -  you may roll now with \"!roll\". You have 30 seconds.")
                                 secondroll = await waitfor(30, who)
-                                if "!roll" in secondroll.content:
+                                if "!roll" in secondroll.content.lower():
                                     roll2 = randint(1, max)
                                     while roll1 == roll2:
                                         roll2 = randint(1, max)
