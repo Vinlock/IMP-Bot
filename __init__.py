a6cbfb5131cff2359a300987807095fa1d00e1b2
Vinlock <dak@pvpallday.com>
2016-04-17 02:50:54 -0700

    Fixed !guess to cost points.

--- __init__.py
+++ __init__.py
@@ -7,15 +7,6 @@ def thread(function):
     t1.daemon = True
     t1.start()
 
-def start():
-    print("Starting Bot...")
-    Bot.Bot()
-
-thread(start)
-
-while True:
-    input(">> ")
-
-# print("Starting Bot...")
-# bot = Bot.Bot()
+print("Starting Bot...")
+bot = Bot.Bot()
 # control = ControlBot.ControlBot()
