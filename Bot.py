56341298588910c3c6f90f058ed68c2b83da7f18
Vinlock <dak@pvpallday.com>
2016-04-17 22:27:31 -0700

    Updated !guess logic to 10%

--- Bot.py
+++ Bot.py
@@ -182,10 +182,10 @@ class Bot(object):
                                  + ("" if self.guess else "**DISABLED:** ") + "**!guess <number 10 or greater>** - The bot will think of a number, if you can guess it you win the jackpot, if you get close you win some points. You bet points equal to the number you choose.\n\n\n")
                 elif command == "color":
                     if self.checkpower(message.author) or self.adminpower(message.author):
-                        rolename = params[1]
+                        rolename = params[2:]
                         if discord.utils.get(message.server.roles, name=rolename):
                             role = discord.utils.get(message.server.roles, name=rolename)
-                            hex = params[2]
+                            hex = params[1]
                             if re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', hex):
                                 try:
                                     int_hex = int(hex, 16)
