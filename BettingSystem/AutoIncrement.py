eb1f28dfc2599b7aa5b6a72298a8731efe7b2a50
Vinlock <dak@pvpallday.com>
2016-04-18 21:55:07 -0700

    Print Markers for Server Increment Bug - Removed the SQL Print

--- BettingSystem/AutoIncrement.py
+++ BettingSystem/AutoIncrement.py
@@ -49,7 +49,7 @@ class Increment(object):
             try:
                 cursor.execute(sql)
                 conn.commit()
-                print("\033[94m" + cursor._last_executed + "\033[0m")
+                # print("\033[94m" + cursor._last_executed + "\033[0m")
                 print("GOOD")
             except:
                 print("Failed to increment list.")
