5edff3f5bf49d1c6c17b7e092f28188909f55a65
Vinlock <dak@pvpallday.com>
2016-04-19 21:13:11 -0700

    Activated logging

--- BotLogging/__init__.py
+++ BotLogging/__init__.py
@@ -8,7 +8,7 @@ def log(*strings):
         s.append(str(string))
     split_string = " ".join(s)
     print(split_string)
-    split_string = time.strftime("%Y-%m-%d %H:%M") + " === " + split_string
+    split_string = time.strftime("%Y-%m-%d %H:%M:%S") + " === " + split_string
     today = datetime.date.today()
     fileName = today.strftime("%Y-%m-%d-%H")
     with open("log/" + fileName + ".log", "a") as myfile:
