428ea6584aef0eb5906ec80e54b0927069ae3a23
Vinlock <dak@pvpallday.com>
2016-04-19 20:11:32 -0700

    Activated logging

--- __init__.py
+++ __init__.py
@@ -1,5 +1,22 @@
 # import ControlBot
-import Bot, threading
+import Bot
+import threading
+import logging
+import datetime
+
+logPath = "log"
+fileName = today = datetime.date.today()
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
 
 
 def thread(function):
